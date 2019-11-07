90c37945085b5f0dd473df520e4bfd88cd9dccf0
MolSSI Education <education@molssi.org>
2019-11-07 15:05:01 -0500

    Improved package structure of molecool.

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,9 @@ A python package for analyzing and visualizing molecules. For the MolSSI Best Pr
 
 # Add imports here
 from .functions import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
 
 # Handle versioneer
 from ._version import get_versions
